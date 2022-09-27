725df7ffa90c36b7608803b143d60c12bf9ab000
Tina Jeong <migujeong@gmail.com>
2022-09-27 10:46:15 +0900

    배치 사이즈 별 테스트 추가

--- README.md
+++ README.md
@@ -1,33 +1,25 @@
 # BulkTest
-
+- batch size : 1000
 ```shell
-insert               |     batch size           |     elasped time        
+insert >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ------------------------------------------------------------
-Regular inserts     |    1         |          30ms (0 s)
-Batch inserts       |    1         |          20ms (0 s)
-Total gain: 33 %
+Regular inserts     |    1         |          27ms (0 s)
+Batch inserts       |    1         |          11ms (0 s)
+Total gain: 59 %
 ------------------------------------------------------------
 Regular inserts     |    10        |           4ms (0 s)
 Batch inserts       |    10        |           2ms (0 s)
 Total gain: 50 %
 ------------------------------------------------------------
-Regular inserts     |    100       |          47ms (0 s)
-Batch inserts       |    100       |          23ms (0 s)
-Total gain: 51 %
+Regular inserts     |    100       |          18ms (0 s)
+Batch inserts       |    100       |           7ms (0 s)
+Total gain: 61 %
 ------------------------------------------------------------
-Regular inserts     |    100       |          47ms (0 s)
-Batch inserts       |    100       |          19ms (0 s)
-Total gain: 59 %
+Regular inserts     |    1000      |         188ms (0 s)
+Batch inserts       |    1000      |          21ms (0 s)
+Total gain: 88 %
 ------------------------------------------------------------
-Regular inserts     |    10000     |        2245ms (2 s)
-Batch inserts       |    10000     |         881ms (0 s)
-Total gain: 60 %
-------------------------------------------------------------
-Regular inserts     |    100000    |       16215ms (16 s)
-Batch inserts       |    100000    |        8741ms (8 s)
-Total gain: 46 %
-------------------------------------------------------------
-Regular inserts     |    1000000   |      167532ms (167 s)
-Batch inserts       |    1000000   |       79840ms (79 s)
-Total gain: 52 %
+Regular inserts     |    10000     |         893ms (0 s)
+Batch inserts       |    10000     |         115ms (0 s)
+Total gain: 87 %
 ```
--- src/test/java/com/example/bulktest/service/BatchUserServiceTest.java
+++ src/test/java/com/example/bulktest/service/BatchUserServiceTest.java
@@ -1,17 +1,92 @@
 package com.example.bulktest.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @SpringBootTest
 @Transactional
 @Slf4j
 class BatchUserServiceIntegrationTest {
 
+    int dataRowCount = 100_000;
+
+    @Autowired
+    SimpleUserService simpleUserService;
     @Autowired
     BatchUserService batchUserService;
 
+    @Test
+    public void 배치사이즈_100() {
+        int batchSize = 100;
+        log.info("insert {} {}", String.join("", Collections.nCopies(30, ">")),batchSize);
+        batchUserService.setBatchSize(batchSize);
+        long normalElapsedTime = simpleUserService.createUser(dataRowCount);
+        long batchElapsedTime = batchUserService.createUser(dataRowCount);
 
+        log.info(String.join("", Collections.nCopies(60, "-")));
+
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Regular inserts", "|", dataRowCount, "|", normalElapsedTime, normalElapsedTime / 1000));
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Batch inserts", "|", dataRowCount, "|", batchElapsedTime, batchElapsedTime / 1000));
+    }
+
+    @Test
+    public void 배치사이즈_500() {
+        int batchSize = 500;
+        log.info("insert {} {}", String.join("", Collections.nCopies(30, ">")),batchSize);
+        batchUserService.setBatchSize(batchSize);
+        long normalElapsedTime = simpleUserService.createUser(dataRowCount);
+        long batchElapsedTime = batchUserService.createUser(dataRowCount);
+
+        log.info(String.join("", Collections.nCopies(60, "-")));
+
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Regular inserts", "|", dataRowCount, "|", normalElapsedTime, normalElapsedTime / 1000));
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Batch inserts", "|", dataRowCount, "|", batchElapsedTime, batchElapsedTime / 1000));
+    }
+
+    @Test
+    public void 배치사이즈_1000() {
+        int batchSize = 1000;
+        log.info("insert {} {}", String.join("", Collections.nCopies(30, ">")),batchSize);
+        batchUserService.setBatchSize(batchSize);
+        long normalElapsedTime = simpleUserService.createUser(dataRowCount);
+        long batchElapsedTime = batchUserService.createUser(dataRowCount);
+
+        log.info(String.join("", Collections.nCopies(60, "-")));
+
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Regular inserts", "|", dataRowCount, "|", normalElapsedTime, normalElapsedTime / 1000));
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Batch inserts", "|", dataRowCount, "|", batchElapsedTime, batchElapsedTime / 1000));
+    }
+
+    @Test
+    public void 배치사이즈_5000() {
+        int batchSize = 5000;
+        log.info("insert {} {}", String.join("", Collections.nCopies(30, ">")),batchSize);
+        batchUserService.setBatchSize(batchSize);
+        long normalElapsedTime = simpleUserService.createUser(dataRowCount);
+        long batchElapsedTime = batchUserService.createUser(dataRowCount);
+
+        log.info(String.join("", Collections.nCopies(60, "-")));
+
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Regular inserts", "|", dataRowCount, "|", normalElapsedTime, normalElapsedTime / 1000));
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Batch inserts", "|", dataRowCount, "|", batchElapsedTime, batchElapsedTime / 1000));
+    }
+
+    @Test
+    public void 배치사이즈_10000() {
+        int batchSize = 10000;
+        log.info("insert {} {}", String.join("", Collections.nCopies(30, ">")),batchSize);
+        batchUserService.setBatchSize(batchSize);
+        long normalElapsedTime = simpleUserService.createUser(dataRowCount);
+        long batchElapsedTime = batchUserService.createUser(dataRowCount);
+
+        log.info(String.join("", Collections.nCopies(60, "-")));
+
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Regular inserts", "|", dataRowCount, "|", normalElapsedTime, normalElapsedTime / 1000));
+        log.info(String.format("%-20s%-5s%-10s%-5s%8sms (%s s)", "Batch inserts", "|", dataRowCount, "|", batchElapsedTime, batchElapsedTime / 1000));
+    }
 }
